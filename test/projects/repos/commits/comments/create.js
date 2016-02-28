ce55035983aec33d2fb6cdea6f28373efd8e13b6
Justin Morgan <justin.l.morgan@gmail.com>
2016-02-28 17:12:00 -0600

    Added support for all the things
    SemVer:Minor

---
+++ test/projects/repos/commits/comments/create.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Third Party
+const defaults = require("lodash/defaults");
+const include = require("include")(__dirname);
+
+// Project
+const stash = include("index"); // Normally: require("stash-client");
+
+// Test
+const stashConfig = include("test/config");
+const config = defaults({
+  limit: 100
+}, stashConfig);
+const projectKey = "TEST";
+const repositorySlug = "test-repo";
+const commitId = "200528952f87fd1e96cdbe98e83432bde857daf1";
+const params = {};
+const values = {
+  anchor: {
+    path: "README.md",
+    srcPath: "README.md"
+  },
+  text: "Test comment"
+};
+
+// 24857 24858 24859 24860 24861
+
+// jscs:disable jsDoc
+stash(config).api().projects().repos(projectKey).commits(repositorySlug).comments(commitId).create(params, values)
+  .then(response => console.log(response.body))
+  .catch(console.error);
