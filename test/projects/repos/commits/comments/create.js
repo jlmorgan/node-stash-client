824cae772d21c0337aac2f7f4f4bd1378e1c2d0b
Justin Morgan <justin.l.morgan@gmail.com>
2018-03-01 12:25:29 -0600

    Issue-13: Fix comments create from incorrectly attaching values
    closes #13
    SemVer:Patch
    bug

--- test/projects/repos/commits/comments/create.js
+++ test/projects/repos/commits/comments/create.js
@@ -17,14 +17,8 @@ const repositorySlug = "test-repo";
 const commitId = "200528952f87fd1e96cdbe98e83432bde857daf1";
 const params = {};
 const values = {
-  anchor: {
-    path: "README.md",
-    srcPath: "README.md"
-  },
   text: "Test comment"
 };
-
-// 24857 24858 24859 24860 24861
 
 // jscs:disable jsDoc
 stash(config).api().projects().repos(projectKey).commits(repositorySlug).comments(commitId).create(params, values)
